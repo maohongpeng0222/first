af45d3a07d0d4e5ff6b510bb424fece0afd5d279
maohongpeng0222 <948209669@qq.com>
2025-01-08 14:21:22 +0800

    da

--- pages/3_📈_文件分析.py
+++ pages/3_📈_文件分析.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import numpy as np
 from datetime import datetime
 import time
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 # 应用标题
 st.title('数据分析平台')
@@ -34,7 +34,7 @@ if uploaded_file is not None:
     train_num = st.sidebar.multiselect('选择列车号', data['列车号'].unique())
 
     start_date = pd.to_datetime(start_date)
-    start_date = st.sidebar.date_input("开始日期:",start_date,format='YYYY/MM/DD')
+    start_date = st.sidebar.date_input("开始日期:", start_date, format='YYYY/MM/DD')
     end_date = st.sidebar.date_input("结束日期:", format='YYYY/MM/DD', key='end_date')
 
     end_date = pd.to_datetime(end_date)
@@ -61,11 +61,11 @@ if uploaded_file is not None:
         # filter_data[filter_data['时间'] >= pd.to_datetime('2024-08-30')]
         # filter_data[pd.to_datetime(filter_data['时间']) <= pd.to_datetime('2024-08-30')]
 
-        df_map = station_info[['纬度','经度']]
-        df_map.rename(columns={'纬度':'lat','经度':'lon'},inplace=True)
-        df_map = pd.DataFrame(df_map,columns=['lat', 'lon'])
+        df_map = station_info[['纬度', '经度']]
+        df_map.rename(columns={'纬度': 'lat', '经度': 'lon'},inplace=True)
+        df_map = pd.DataFrame(df_map, columns=['lat', 'lon'])
 
-        tab1, tab2 = st.tabs(['地图','统计'])
+        tab1, tab2 = st.tabs(['地图', '统计'])
         with tab1:
             st.map(df_map)
         with tab2:
@@ -74,12 +74,12 @@ if uploaded_file is not None:
             st.bar_chart(num_DBX.head(20))
 
 else:
-    tab1, tab2 = st.tabs(['地图','统计'])
+    tab1, tab2 = st.tabs(['地图', '统计'])
     with tab1:
         st.map()
     with tab2:
         st.bar_chart()
 
-    train_num = st.sidebar.multiselect('选择列车号',[])
-    start_date = st.sidebar.date_input("开始日期:",start_date, format='YYYY/MM/DD', key='start_date')
+    train_num = st.sidebar.multiselect('选择列车号', [])
+    start_date = st.sidebar.date_input("开始日期:", start_date, format='YYYY/MM/DD', key='start_date')
     end_date = st.sidebar.date_input("结束日期:", format='YYYY/MM/DD', key='end_date')
